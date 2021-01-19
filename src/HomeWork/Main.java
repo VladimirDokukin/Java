29696be457490d86d456ab90450edf3e6da5fc91
VladimirDokukin <VladimirDokukin57@yandex.ru>
2021-01-19 23:20:39 +0300

    return

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -7,18 +7,17 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // write your code here
 
-        //FunctionSolve();
-        //SecondTask();
-        //ThirdTask();
-        //FourthTask();
-        FifthTask();
+        //System.out.println(FunctionSolve());
+        //System.out.println(SecondTask());
+        //System.out.println(ThirdTask());
+        //System.out.println(FourthTask());
+        System.out.println(FifthTask());
+
         scanner.close();
-
     }
 
-    public static void FunctionSolve() {
+    public static float FunctionSolve() {
         int a, b, c, d;
         System.out.println("Решение - a * (b + (c/d))\nВведите значение a");
         a = scanner.nextInt();
@@ -31,10 +30,10 @@ public class Main {
 
         float nd = (float) c / d;
         float result = a * (b + nd);
-        System.out.println(result);
+        return result;
     }
 
-    public static void SecondTask() {
+    public static boolean SecondTask() {
         int a, b, c;
         System.out.println("Введите первое целое число");
         a = scanner.nextInt();
@@ -44,39 +43,41 @@ public class Main {
 
         if (10 <= c && c <= 20) {
             System.out.println("Число " + c + " лежит в диапазоне от 10 до 20");
+            return true;
         } else {
             System.out.println("Число " + c + " не лежит в диапазоне от 10 до 20");
+            return false;
         }
     }
 
-    public static void ThirdTask() {
+    public static String ThirdTask() {
         int a;
         System.out.println("Введите целое число");
         a = scanner.nextInt();
         if (a >= 0) {
-            System.out.println("Введеное число положительное: " + a);
+            return ("Введеное число положительное: " + a);
         } else {
-            System.out.println("Введеное число отрицательное: " + a);
+            return ("Введеное число отрицательное: " + a);
         }
     }
 
-    public static void FourthTask() {
+    public static String FourthTask() {
         String name;
         System.out.println("Введите строку");
         name = scanner.nextLine();
-        System.out.println("Привет, " + name + "!");
+        return  ("Привет, " + name + "!");
     }
 
-    public static void FifthTask() {
+    public static String FifthTask() {
         int year;
         System.out.println("Введите год");
         year = scanner.nextInt();
         if ((year % 4) == 0 && (year % 100) != 0) {
-            System.out.println("Год високосный: " + year);
+            return ("Год високосный: " + year);
         }
      else {
-            if (year % 100 == 0 && (year % 400) == 0) { System.out.println("Год високосный: " + year);}
-            else System.out.println("Год не високосный: " + year);
+            if (year % 100 == 0 && (year % 400) == 0) { return ("Год високосный: " + year);}
+            else return ("Год не високосный: " + year);
     }
 
     }
