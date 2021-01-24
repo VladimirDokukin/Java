871b6d97a7a3eb3ab5ed67df4526c6a3ad2e2d37
VladimirDokukin <VladimirDokukin57@yandex.ru>
2021-01-24 12:46:40 +0300

    Start

---
+++ Lesson3/src/Lesson3/HomeWork/Main.java
@@ -0,0 +1,13 @@
+package Lesson3.HomeWork;
+
+public class Main {
+
+    private static final char HUMAN_SHIFT = 'X';
+    private static final char MACHINE_SHIFT = 'O';
+    private static final int xFieldLine = 3,yFieldLine = 3;
+    private static int [][] gameField = new int[xFieldLine][yFieldLine];
+
+    public static void main(String[] args) {
+
+    }
+}
